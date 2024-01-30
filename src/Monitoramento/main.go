8e9693391e6adbb427ee9848612d2f43f57414e1
Perazzojoao <perazzojoao@gmail.com>
2024-01-30 15:57:10 -0300

    Adição de escrita em arquivo

--- src/Monitoramento/main.go
+++ src/Monitoramento/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -27,7 +28,7 @@ func main() {
 		case 1:
 			iniciarMonitoramento()
 		case 2:
-			fmt.Println("Exibindo Logs...")
+			exibeLogs()
 		case 0:
 			fmt.Println("Saindo do Programa...")
 			os.Exit(0)
@@ -117,20 +118,55 @@ func testaSite(i int, site string) {
 
 	fmt.Print("Site ", i+1, ": ")
 	if resp.StatusCode == 200 {
-		registraLogs(site, true)
+		registraLogs(i, site, true)
 		fmt.Println(site, "-> Carregado com sucesso! StatusCode:", resp.StatusCode)
 	} else {
-		registraLogs(site, false)
+		registraLogs(i, site, false)
 		fmt.Println(site, "-> Fora do ar! StatusCode:", resp.StatusCode)
 	}
 }
 
-func registraLogs(site string, status bool) {
-	file, err := os.OpenFile("logs.txt", os.O_RDWR|os.O_CREATE, 0666)
+func registraLogs(i int, site string, status bool) {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE, 0666)
 
 	if err != nil {
 		fmt.Println("Erro ao escrever em arquivo -> Erro:", err)
 	}
 
-	fmt.Println(file)
+	file.WriteString(time.Now().Format("02/01/2006 15:04:05 - "))
+	file.WriteString("Site " + strconv.FormatInt(int64(i+1), 10) + ": ")
+
+	if status == true {
+		file.WriteString(site + " -> Site online\n")
+	} else {
+		file.WriteString(site + " -> Site offline\n")
+	}
+	if i == monitoramentos+1 {
+		file.WriteString("\n")
+	}
+	file.Close()
+}
+
+func exibeLogs() {
+	fmt.Println("Exibindo Logs...")
+	println()
+
+	file, err := os.Open("logs.txt")
+
+	if err != nil {
+		fmt.Println("Incapaz de abrir o arquivo: logs.txt -> Erro:", err)
+	}
+
+	leitor := bufio.NewReader(file)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+
+		if err == io.EOF {
+			break
+		}
+		linha = strings.TrimSpace(linha)
+		fmt.Println(linha)
+	}
+	file.Close()
 }

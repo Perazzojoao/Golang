87119b01e138676db8f2e97853325d4914675daf
Perazzojoao <perazzojoao@gmail.com>
2024-02-04 17:50:37 -0300

    Projeto Finalizado

--- src/Alura/Api-go/controllers/produtos.go
+++ src/Alura/Api-go/controllers/produtos.go
@@ -9,7 +9,6 @@ import (
 	"strconv"
 
 	"api-web/models"
-
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -48,5 +47,47 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	models.AddNewProduct(nome, descricao, precoConvertido, estoqueConvertido)
+	http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	idProduto := r.URL.Query().Get("id")
+
+	models.DeletaProduto(idProduto)
+	http.Redirect(w, r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	produto := models.GetProduto(r.URL.Query().Get("id"))
+
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		err := errors.New("ERROR: You suposed to meke a PUT request.")
+		fmt.Println(err.Error())
+		http.Redirect(w, r, "/", 401)
+		return
+	}
+
+	id := r.FormValue("id")
+	nome := r.FormValue("nome")
+	descricao := r.FormValue("descricao")
+	preco := r.FormValue("preco")
+	estoque := r.FormValue("estoque")
+
+	precoConvertido, err := strconv.ParseFloat(preco, 64)
+	if err != nil {
+		log.Println("Error trying to convert 'preco':", err.Error())
+		return
+	}
+	estoqueConvertido, err := strconv.Atoi(estoque)
+	if err != nil {
+		log.Println("Error trying to convert 'estoque'", err.Error())
+		return
+	}
+
+	models.UpdateProduto(id, nome, descricao, precoConvertido, estoqueConvertido)
 	http.Redirect(w, r, "/", 301)
 }
--- src/Alura/Api-go/models/produto.go
+++ src/Alura/Api-go/models/produto.go
@@ -2,9 +2,9 @@ package models
 
 import (
 	"fmt"
+	"log"
 
 	"api-web/db"
-
 )
 
 type Produto struct {
@@ -19,7 +19,7 @@ func SelectAllProducts() []Produto {
 	db := db.ConectarDb()
 	defer db.Close()
 
-	selectAll, err := db.Query("SELECT * FROM produtos")
+	selectAll, err := db.Query("SELECT * FROM produtos ORDER BY id ASC")
 	if err != nil {
 		fmt.Println("ERROR -> Unable to perform a query")
 		fmt.Println(err.Error())
@@ -61,4 +61,70 @@ func AddNewProduct(nome string, descricao string, preco float64, estoque int) {
 		return
 	}
 	stmt.Exec(nome, descricao, preco, estoque)
+	stmt.Close()
+	log.Printf("Product '%s' added to data base.\n", nome)
+}
+
+func DeletaProduto(id string) {
+	db := db.ConectarDb()
+	defer db.Close()
+
+	stmt, err := db.Prepare("DELETE FROM alura_loja.produtos WHERE id = ?")
+	if err != nil {
+		fmt.Println("ERROR -> Wrong delete arguments.")
+		fmt.Println(err.Error())
+		return
+	}
+	stmt.Exec(id)
+	stmt.Close()
+
+	log.Println("Product ID =", id, "deleted.")
+}
+
+func GetProduto(idProduto string) Produto {
+	db := db.ConectarDb()
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT * FROM alura_loja.produtos WHERE id = ?")
+	if err != nil {
+		fmt.Println("ERROR -> wrong select arguments.")
+		fmt.Println(err.Error())
+		return Produto{}
+	}
+	product, err := stmt.Query(idProduto)
+	if err != nil {
+		fmt.Println("ERROR -> Unable to perform the query.")
+		fmt.Println(err.Error())
+		return Produto{}
+	}
+	stmt.Close()
+
+	var id, estoque int
+	var nome, descricao string
+	var preco float64
+
+	product.Next()
+	err = product.Scan(&id, &nome, &descricao, &preco, &estoque)
+	if err != nil {
+		fmt.Println("ERROR -> Unable to scan table line")
+		fmt.Println(err.Error())
+	}
+
+	return Produto{id, nome, descricao, preco, estoque}
+}
+
+func UpdateProduto(id string, nome string, descricao string, preco float64, estoque int) {
+	db := db.ConectarDb()
+	defer db.Close()
+
+	stmt, err := db.Prepare("UPDATE alura_loja.produtos SET nome = ?, descricao = ?, preco = ?, estoque = ? WHERE id = ?")
+	if err != nil {
+		fmt.Println("ERROR -> wrong update arguments.")
+		fmt.Println(err.Error())
+		return
+	}
+
+	stmt.Exec(nome, descricao, preco, estoque, id)
+	stmt.Close()
+	log.Println("Produto id =", id, "updated.")
 }
